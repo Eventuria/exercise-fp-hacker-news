b887a8b2ce8ad0c5829018f10f5945fdac3e9e3e
Nicolas Henin <mr.henin@gmail.com>
2019-04-23 15:26:56 +0200

    handle hacker news api inconsistencies...

--- top-commenters-cli/src/main/scala/eventuria/Executable.scala
+++ top-commenters-cli/src/main/scala/eventuria/Executable.scala
@@ -34,20 +34,20 @@ object HackerNewsDomain {
 
     override def toString: String = {
       val toStringTopCommenter :String = topCommenter match {
-        case topCommenter if topCommenter.size == 0  => "No Commenter yet\n"
+        case topCommenter if topCommenter.isEmpty  => "No Commenter yet\n"
         case topCommenter => Monoid.combineAll[String](
             topCommenter.
               zipWithIndex.
               map {case ((nbComments,users),index) => {
                 val userList :String =  Monoid.combineAll[String](users.toList.map(_ ++ " | "))
-                s"\t${"%3d".format(index+1)}  - $nbComments comment(s) - ${userList} \n"}})
+                s"\n\t${"%3d".format(index+1)}  - $nbComments comment(s) - $userList "}}) ++ "\n"
       }
 
-      s"---------------------------------\n" ++
-      s"∙∙∙ Story ∙  $id ∙  $title\n" ++
-      s"‣ Total Comments : $totalComments\n" ++
+      s"\n" ++
+      s"∙∙∙ Story ∙ $id - $title\n" ++
+      s"‣ Total Comments : $totalComments\n (${totalComments +1} api calls to hacker news api" ++
       s"‣ Total Anonymous Comments :$totalAnonymousComments \n" ++
-      s"‣ Top Commenters :\n" ++
+      s"‣ Top Commenters :" ++
        toStringTopCommenter
     }
   }
@@ -86,30 +86,28 @@ object JsonDecoding {
   import HackerNewsDomain._
 
   implicit val storyDecoder : Decoder[Story] = Decoder.instance (json =>
-    for {
-      id <- json.downField("id").as[StoryId]
-      title <- json.downField("title").as[String]
-      commentIdsOption <- json.downField("kids").as[Option[NonEmptyList[CommentId]]]
-    }
-    yield { commentIdsOption match {
-      case None  => UncommentedStory (id,title)
-      case Some (commentIds) if commentIds.size == 0 => UncommentedStory (id,title)
-      case Some (commentIds) if commentIds.size > 0 => CommentedStory (id,title,commentIds)}})
+      for {
+        id <- json.downField("id").as[StoryId]
+        title <- json.downField("title").as[String]
+        commentIdsOption <- json.downField("kids").as[Option[NonEmptyList[CommentId]]]}
+      yield { commentIdsOption match {
+          case None  => UncommentedStory (id,title)
+          case Some (commentIds) if commentIds.size == 0 => UncommentedStory (id,title)
+          case Some (commentIds) if commentIds.size > 0 => CommentedStory (id,title,commentIds)}})
 
   implicit val commentDecoder : Decoder[Comment] = Decoder.instance (json =>
     for {
       id <- json.downField("id").as[StoryId]
       userOption <- json.downField("by").as[Option[String]]
-      commentIdsOption <- json.downField("kids").as[Option[NonEmptyList[CommentId]]]
-    }
-      yield { (userOption,commentIdsOption) match {
-        case (None, None)  => UncommentedAnonymousComment (id)
-        case (None,Some (commentIds)) if commentIds.size == 0 => UncommentedAnonymousComment (id)
-        case (None,Some (commentIds)) if commentIds.size > 0 => AnonymousComment (id,commentIds)
-        case (Some(user), None)  => UncommentedIdentifiedComment (id,user)
-        case (Some(user),Some (commentIds)) if commentIds.size == 0 => UncommentedIdentifiedComment (id,user)
-        case (Some(user),Some (commentIds)) if commentIds.size > 0 => IdentifiedComment (id,user,commentIds)}})
-
+      commentIdsOption <- json.downField("kids").as[Option[NonEmptyList[CommentId]]]}
+    yield { (userOption,commentIdsOption) match {
+      case (None, None)  => UncommentedAnonymousComment (id)
+      case (None,Some (commentIds)) if commentIds.size == 0 => UncommentedAnonymousComment (id)
+      case (None,Some (commentIds)) if commentIds.size > 0 => AnonymousComment (id,commentIds)
+      case (Some(user), None)  => UncommentedIdentifiedComment (id,user)
+      case (Some(user),Some (commentIds)) if commentIds.size == 0 => UncommentedIdentifiedComment (id,user)
+      case (Some(user),Some (commentIds)) if commentIds.size > 0 => IdentifiedComment (id,user,commentIds)}}
+  )
 }
 
 object TopCommenterApp extends IOApp {
@@ -134,20 +132,29 @@ object TopCommenterApp extends IOApp {
       }
 
 
-    def streamComments (commentId: CommentId) : Stream [IO, Comment] = {
+    def streamComments (commentId: CommentId) : Stream [IO, Either[String,Comment]] = {
         Stream
           .emit[IO,CommentId](commentId)
-          .evalMap[IO,Comment](commentId => httpClient.expect[Comment](baseUrl / "v0" / "item" / s"$commentId.json"))
+          .evalMap[IO,Either[String,Comment]](commentId =>
+            httpClient
+              .expect[Comment](baseUrl / "v0" / "item" / s"$commentId.json")
+              .map(comment => comment.asRight[String])
+              .handleErrorWith { e =>
+                for {
+                  _ <- IO(println (s"\n Fetching Comment ($commentId) failed (try : ${baseUrl / "v0" / "item" / s"$commentId.json"} ) : ${e}"))
+                } yield Left(e.toString)
+                })
           .flatMap {
-            case comment @IdentifiedComment(_, _, comments) =>
+            case Left(e) => Stream.emit[IO,Either[String,Comment]](Left(e))
+            case Right(comment @IdentifiedComment(_, _, comments)) =>
               Stream
-                .emit[IO,Comment](comment)
+                .emit[IO,Either[String,Comment]](comment.asRight)
                 .merge(Stream.emits[IO,CommentId](comments.toList).flatMap(streamComments))
 
             // Recursion Base Cases
-            case comment @AnonymousComment(_, _)            => Stream.emit[IO,Comment](comment)
-            case comment @UncommentedIdentifiedComment(_,_) => Stream.emit[IO,Comment](comment)
-            case comment @UncommentedAnonymousComment(_)    => Stream.emit[IO,Comment](comment)
+            case Right(comment @AnonymousComment(_, _))            => Stream.emit[IO,Either[String,Comment]](Right (comment))
+            case Right(comment @UncommentedIdentifiedComment(_,_)) => Stream.emit[IO,Either[String,Comment]](Right (comment))
+            case Right(comment @UncommentedAnonymousComment(_))    => Stream.emit[IO,Either[String,Comment]](Right (comment))
           }
 
 
@@ -160,35 +167,37 @@ object TopCommenterApp extends IOApp {
             Stream.emits[IO,CommentId](comments.toList)
               .flatMap(commentId => streamComments(commentId))
               .compile
-              .fold(TopCommenterStoryBuilder(id, title))( (builder,newComment) => newComment match {
-                case IdentifiedComment(_, user, _) =>
-                  TopCommenterStoryBuilder(
-                    id,
-                    title,
-                    topCommenter = builder.topCommenter + (user -> (builder.topCommenter.getOrElse(user,0) + 1)) ,
-                    totalAnonymousComments = builder.totalAnonymousComments,
-                    totalComments = builder.totalComments + 1)
-                case UncommentedIdentifiedComment(_, user) =>
-                  TopCommenterStoryBuilder(
-                    id,
-                    title,
-                    topCommenter = builder.topCommenter + (user -> (builder.topCommenter.getOrElse(user,0) + 1)) ,
-                    totalAnonymousComments = builder.totalAnonymousComments,
-                    totalComments = builder.totalComments + 1)
-                case AnonymousComment(_,_) =>
-                  TopCommenterStoryBuilder(
-                    id,
-                    title,
-                    topCommenter = builder.topCommenter ,
-                    totalAnonymousComments = builder.totalAnonymousComments +1,
-                    totalComments = builder.totalComments + 1)
-                case UncommentedAnonymousComment(_) =>
-                  TopCommenterStoryBuilder(
-                    id,
-                    title,
-                    topCommenter = builder.topCommenter ,
-                    totalAnonymousComments = builder.totalAnonymousComments +1,
-                    totalComments = builder.totalComments + 1)})
+              .fold(TopCommenterStoryBuilder(id, title))( (builder,newCommentFetched) => newCommentFetched match {
+                case Left(_) => builder
+                case Right(comment) => comment match {
+                  case IdentifiedComment(_, user, _) =>
+                    TopCommenterStoryBuilder(
+                      id,
+                      title,
+                      topCommenter = builder.topCommenter + (user -> (builder.topCommenter.getOrElse(user,0) + 1)) ,
+                      totalAnonymousComments = builder.totalAnonymousComments,
+                      totalComments = builder.totalComments + 1)
+                  case UncommentedIdentifiedComment(_, user) =>
+                    TopCommenterStoryBuilder(
+                      id,
+                      title,
+                      topCommenter = builder.topCommenter + (user -> (builder.topCommenter.getOrElse(user,0) + 1)) ,
+                      totalAnonymousComments = builder.totalAnonymousComments,
+                      totalComments = builder.totalComments + 1)
+                  case AnonymousComment(_,_) =>
+                    TopCommenterStoryBuilder(
+                      id,
+                      title,
+                      topCommenter = builder.topCommenter ,
+                      totalAnonymousComments = builder.totalAnonymousComments +1,
+                      totalComments = builder.totalComments + 1)
+                  case UncommentedAnonymousComment(_) =>
+                    TopCommenterStoryBuilder(
+                      id,
+                      title,
+                      topCommenter = builder.topCommenter ,
+                      totalAnonymousComments = builder.totalAnonymousComments +1,
+                      totalComments = builder.totalComments + 1)}})
               .map (_.buildWithFirstXTopCommenter(xFirstCommenter))
         }
     }
